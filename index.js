b00bb79e3d8436a1c16dbc936cd5178bf9da501d
Papa403 <alilykai@gmail.com>
2025-08-13 17:56:10 -0500

    connected mongodb to app

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -15,23 +17,25 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+// let notes = [
+//   {
+//     id: "1",
+//     content: "HTML is easy",
+//     important: true
+//   },
+//   {
+//     id: "2",
+//     content: "Browser can execute only JavaScript",
+//     important: false
+//   },
+//   {
+//     id: "3",
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true
+//   }
+// ]
+
+let notes = []
 
 const generateId = () => {
   const maxId = notes.length > 0
@@ -49,15 +53,14 @@ app.post('/api/notes',(request, response) => {
     })
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/',(request, response) => {
@@ -65,17 +68,15 @@ app.get('/',(request, response) => {
 })
 
 app.get('/api/notes',(request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id',(request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if(note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id',(request, response) => {
@@ -91,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
