a8297a0a9b301d1cc5a03b6e6b651e0ae6818666
Papa403 <alilykai@gmail.com>
2025-08-14 13:45:24 -0500

    implemented proper GET DELETE and POST logic for index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.json())
-app.use(express.static('dist'))
+let notes = []
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -15,34 +14,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('dist'))
+app.use(express.json())
 app.use(requestLogger)
-
-// let notes = [
-//   {
-//     id: "1",
-//     content: "HTML is easy",
-//     important: true
-//   },
-//   {
-//     id: "2",
-//     content: "Browser can execute only JavaScript",
-//     important: false
-//   },
-//   {
-//     id: "3",
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true
-//   }
-// ]
-
-let notes = []
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes',(request, response) => {
   const body = request.body
@@ -73,17 +47,43 @@ app.get('/api/notes',(request, response) => {
   })
 })
 
-app.get('/api/notes/:id',(request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id',(request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id',(request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id',(request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -91,6 +91,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
