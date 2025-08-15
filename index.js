92e7da6fc282bf542f399199c69cc2fbeff9339e
Papa403 <alilykai@gmail.com>
2025-08-14 22:22:52 -0500

    implemented eslint

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const Note = require('./models/note')
 
 const app = express()
 
-let notes = []
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path: ', request.path)
@@ -29,8 +27,8 @@ app.post('/api/notes',(request, response, next) => {
   note.save()
     .then(savedNote => {
       response.json(savedNote)
-  })
-  .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.get('/',(request, response) => {
@@ -45,20 +43,20 @@ app.get('/api/notes',(request, response) => {
 
 app.get('/api/notes/:id',(request, response, next) => {
   Note.findById(request.params.id)
-  .then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id',(request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
@@ -83,7 +81,7 @@ app.put('/api/notes/:id', (request, response, next) => {
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -92,9 +90,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if(error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   } else if(error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
